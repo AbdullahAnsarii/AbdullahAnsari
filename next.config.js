6c9c0098c0caf1cece75f83ff183ed8d3e0ec79c
Abdullah Ansari <aa9272137@gmail.com>
2022-07-14 02:14:11 +0500

    deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  images:{
+    loader: 'akamai',
+    path: ""
+  }
 }
-
-module.exports = nextConfig
